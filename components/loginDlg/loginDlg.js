686e63f9603a6ccee625b10e140ea4104fd6841c
hqfeijian <1661044722@qq.com>
2020-04-30 09:30:32 +0800

    兼容小程序老版本1.9.9-2.2.2

--- components/loginDlg/loginDlg.js
+++ components/loginDlg/loginDlg.js
@@ -1,4 +1,6 @@
 // components/login/loginDlg.js
+import util from '../../utils/util.js';
+
 Component({
   /**
    * 组件的属性列表
@@ -14,24 +16,42 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    canIUseGetUserInfo:wx.canIUse('button.open-type.getUserInfo')
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    returnMove:function(){
+      return
+    },
     clickMask:function() {
       this.setData({show: false})
+      var that = this;
+      console.log('clickMask');
+      //此处调用时为了兼容1.9.9-2.2.2之间的版本，因为这之间的版本不会响应loginDlg事件，原因不明，但是会响应clickMask
+      wx.getUserInfo({
+        success(res){
+          console.log('getUserInfo res=',res)
+          if(res.userInfo){
+            var loginInfo = {
+              detail:res
+            };
+            that.loginDlg(loginInfo);
+          }
+        }
+      })
     },
 
     cancelDlg: function () {
+      console.log("cancelDlg")
       this.setData({ show: false })
       this.triggerEvent('cancel')
     },
 
     loginDlg: function (e) {
-      console.log("login", e)
+      console.log("loginDlg")
       this.setData({ show: false })
       this.triggerEvent('login',e)
     }
